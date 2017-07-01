a8f0d3e99fbfc53f20827104224af42e20ae6f1f
wencakisa <wencakisa@gmail.com>
2017-07-01 15:08:15 +0300

    Added Heroku configuration

--- mashup/settings.py
+++ mashup/settings.py
@@ -133,10 +133,27 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Default redirect URL
 
 LOGIN_REDIRECT_URL = '/'
 
+
 # Mute SSL warnings
+
 import urllib3
 urllib3.disable_warnings()
+
+
+# Update database configuration with $DATABASE_URL.
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+

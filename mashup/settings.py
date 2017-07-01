d11115f86f22e04b6eb89ee846e40e91d880d8ee
wencakisa <wencakisa@gmail.com>
2017-07-01 15:15:45 +0300

    Removed media configurations

--- mashup/settings.py
+++ mashup/settings.py
@@ -133,9 +133,6 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Default redirect URL
 
--- mashup/urls.py
+++ mashup/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'', include('website.urls', namespace='website', app_name='website')),
     url(r'', include('users.urls', namespace='users', app_name='users')),
     url(r'^events/', include('events.urls', namespace='events', app_name='events'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.SETTINGS_URL, document_root=settings.SETTINGS_ROOT)

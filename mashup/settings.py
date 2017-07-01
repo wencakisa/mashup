81810a80b35565db15b15d4b3cfb26b9080e0a55
wencakisa <wencakisa@gmail.com>
2017-07-01 15:12:38 +0300

    Changed static files config

--- mashup/settings.py
+++ mashup/settings.py
@@ -124,10 +124,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 MEDIA_URL = '/media/'
